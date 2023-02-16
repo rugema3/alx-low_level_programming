51fa9191c668b3231134471fb24e1ec8f8af8301
rugema3 <rugema61@gmail.com>
2023-02-16 12:24:32 -0800

    outputing using hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - this is a program that just prints 
+ *the main is the entry point
+ *return: 0 thi closes the program
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle ");
+return (0);
+}
