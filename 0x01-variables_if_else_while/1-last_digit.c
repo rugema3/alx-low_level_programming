5c7db2589453daaf87d64f54418dc18ae64e36ce
rugema3 <rugema61@gmail.com>
2023-02-17 12:54:45 -0800

    printing a last digit of a random num

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-	else
+	else if ((n % 10) == 0)
 	{
 		printf("last digit of %d is %d and is 0\n", n, n % 10);
 	}
