f4552cf916ab0726118d2a592f7f3ba85d91090b
rugema3 <rugema61@gmail.com>
2023-02-17 14:46:14 -0800

    printing alphabet lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - this program prints alphabets in lowercase.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+
+}
