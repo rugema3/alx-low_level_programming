80e8714f2a5ee6c925bd39d9a93aff563e7b5f3c
rugema3 <rugema61@gmail.com>
2023-02-17 17:36:38 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num = 48; /* representing zero in decimal*/
+
+	while (num <= 102) /* 102 is the representation of f*/
+	{
+		putchar(num);
+		if (num == 57)
+		{
+			num += 39;
+		}
+		num++;
+	}
+		putchar('\n');
+		return (0);
