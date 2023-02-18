59158946e50b8506f675b5eb9e0156dad6cf330a
rugema3 <rugema61@gmail.com>
2023-02-17 16:00:16 -0800

    alphabet reverse order

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - this program prints alphabets in lowercase.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+
+}
