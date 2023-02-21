0eb417a6fbadf728749988aa52b249aa1cf316e7
rugema3 <rugema61@gmail.com>
2023-02-21 23:11:49 +0000

    printing fibbonaci 1st 50 numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ * separated by a comma followed by a space.
+ *
+ * Return: 0.(success)
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		float total;
+
+		while (1)
+		{
+			sum = fib1 + fib2;
+
+			if (sum > 4000000)
+				break;
+			if ((sum %)2 == 0)
+				total + = sum;
+
+			fib1 = fib2;
+			fib2 = sum;
+		}
+		printf("%.0f\n", total_sum);
+
+					return (0);
+}
