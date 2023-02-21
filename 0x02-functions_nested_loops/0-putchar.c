a6a6cb39f0418467a67844dba108ff6f09b18dd9
rugema3 <rugema61@gmail.com>
2023-02-21 11:53:52 +0000

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - the entry point:This program prints _putchar, followed by a new line
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+
+}
+
+
