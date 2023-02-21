b5984dd728dacbd9a367aeac60c820dfc82b0376
rugema3 <rugema61@gmail.com>
2023-02-21 13:03:39 +0000

    function to print the lowercases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+
+/**
+ * print_alphabet - this function prints alphabets in lowercase using _putchar.
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
