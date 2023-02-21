1c1faee4aef2f41227717c467c007fd4b753d820
rugema3 <rugema61@gmail.com>
2023-02-21 19:26:01 +0000

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet lowercase
+ * return ;0 ( success)
+ */
+#include<main.h>
+
+void print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		a++;
+		char ch = 'a'
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+	}
+}
