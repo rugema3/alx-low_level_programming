0fc1f7a64d3c373f0b966955ca3ae717990a071a
rugema3 <rugema61@gmail.com>
2023-02-22 01:32:22 +0000

    multiplication tables of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include"main.h"
+/**
+ * times_table _ prints 9 multiplication table
+ * Description: This program prints the multiplication table of 9
+ * Return: void
+ */
+void times_table(void)
+{
+	int row, column, tens, ones;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 0; column++)
+		{
+			int product = row * column;
+
+			tens = product / 10;
+			ones = product % 10;
+			if (column == 0)
+			{
+				_putchat('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+
