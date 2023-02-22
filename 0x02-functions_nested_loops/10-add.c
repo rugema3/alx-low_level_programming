ca10e817854aff5c857c04de0f6ad0e93b280b41
rugema3 <rugema61@gmail.com>
2023-02-22 00:47:02 +0000

    adding wto integers and return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * add - adding two integers
+ * @int: any integer to be added
+ * Return: it will return the value of the addition
+ *
+ */
+
+int add(int, int)
+{
+	int a, b;
+
+	sum = a + b;
+	return (sum);
+}
