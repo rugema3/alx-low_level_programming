c530b5710be150093246a441ac2cb9117816be4f
rugema3 <rugema61@gmail.com>
2023-02-22 01:34:01 +0000

    multiplication tables of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,13 @@
  */
 void times_table(void)
 {
-	int row, column, tens, ones;
+	int row, column, product, tens, ones;
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 0; column++)
 		{
-			int product = row * column;
+			product = row * column;
 
 			tens = product / 10;
 			ones = product % 10;
