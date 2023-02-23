c6e9f7a0eb2077cdd6ca3492612a6267a6c9a371
rugema3 <rugema61@gmail.com>
2023-02-23 22:01:43 +0000

    printing a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - checking for a digit
+ * @size : integer type
+ * Return:void
+ */
+
+
+void print_triangle(int size)
+{
+	int i = 1, a;
+
+	while (i <= size && size > 0)
+	{
+		a = 0;
+
+		while (a < size - i)
+		{
+			_putchar(' ');
+			a++;
+		}
+		a = 0;
+		while (a < i)
+		{
+			_putchar('#');
+			a++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
+}
