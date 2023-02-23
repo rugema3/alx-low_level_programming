4bf3167072bb1976394fffa187657e2de27d525f
rugema3 <rugema61@gmail.com>
2023-02-23 18:34:19 +0000

    Fizz buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+/**
+ * main - Fizz Buzz test
+ * Return: 0 (success)
+ *
+ */
+#include<stdio.h>
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
