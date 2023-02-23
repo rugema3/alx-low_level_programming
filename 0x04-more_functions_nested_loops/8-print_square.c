290e36e7651acf888b967c57f6062ab8ad67293d
rugema3 <rugema61@gmail.com>
2023-02-23 21:49:12 +0000

    printing a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - check for a digit
+ * @n: number of _ to be printed
+ * Return: void
+ */
+
+void print_square(int n)
+{
+	int i = 0, a;
+
+	while (i < n && n > 0)
+	{
+		a = 0;
+
+		while (a < n)
+		{
+			_putchar('#');
+			a++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
+
