07e50679c3ffc9b82e8dc62fbd23e9a50afa473c
rugema3 <rugema61@gmail.com>
2023-02-23 11:31:00 +0000

    Product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * mul - mulitipliying two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: it will return the value of the addition
+ */
+
+int mul(int a, int b)
+{
+	int producr = a * b;
+
+	return (sum);
+}
