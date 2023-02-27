6e132e17ef86f7f547a004f7fa5d77f43004cdc6
rugema3 <rugema61@gmail.com>
2023-02-27 15:47:11 +0000

    swapping two numbers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - updating the value assigned to a pointer
+ * @n: integer pointer of the value to be updated
+ *
+ */
+
+#include"main.h"
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
