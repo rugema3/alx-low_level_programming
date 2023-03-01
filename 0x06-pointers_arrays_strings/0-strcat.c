11a48e73d7930b391275713335d24b8aa1080428
rugema3 <rugema61@gmail.com>
2023-03-01 13:33:17 -0800

    concantenating two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+/**
+ * _strcat - function that concatenates
+ *          two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	for (j = 0; src[j] ; j++)
+	{
+		dest[i++] = src[j];
+	}
+	return (dest);
+}
