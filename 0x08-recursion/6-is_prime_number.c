4b1389973572db7312ae19d7c21db49e85600877
rugema3 <rugema61@gmail.com>
2023-03-08 14:11:01 -0500

    checking if a number if prime or not with recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,34 @@
 /**
+ * check_num - checking the number
+ * @num:integer
+ * @i:iterator
+ * Return:1 or 0
+ */
+int check_num(int num, int i)
+{
+	if (i == num - 1)
+		return (1);
+	else if (num % i == 0)
+		return (0);
+	if (num % i != 0)
+		return (check_num(num, i + 1));
+	return (0);
+}
+
+/**
  * is_prime_number - a function that returns 1 if the input integer
  * is a prime number, otherwise return 0.
  * @n: integer
  * Return: 1 if the number is prime, 0 is not prime
  *
  */
-#include<stdio.h>
 int is_prime_number(int n)
 {
-	i= n / 2;
-	if(i==1){
-    return 1;
-  }else{
-    if(num%i==0) {
-      return 0;
-    } else {
-      i = i-1;
-      is_prime_number(num);
-    }
-  }
+	int i = 2;
+
+	if (n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (check_num(n, i));
+}
