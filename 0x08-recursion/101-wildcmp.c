b3b972a65e4207cdc641eafa295be50753b11287
rugema3 <rugema61@gmail.com>
2023-03-08 15:45:14 -0500

    a function that returns 1 if a string is a palindrome and 0 if not

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+/**
+ * wildcmp - comparing strings
+ * @s1: pointer1 to the string parameter
+ * @s2:pointer2 to the string parameter
+ * Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
