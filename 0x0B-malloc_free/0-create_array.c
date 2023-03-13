1865470e771eab9a7c083dff19c209ec5ded40da
rugema3 <rugema61@gmail.com>
2023-03-13 05:56:11 -0400

    complying with betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars
+ * and initializes it with a specific char
  * @size: the size of the array to create
  * @c: the char to initialize the array with
  *
@@ -10,20 +11,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-        char *array;
-        unsigned int i;
+	char *array;
+	unsigned int i;
 
-        if (size == 0) /* If size is zero, return NULL */
-                return (NULL);
+	if (size == 0) /* If size is zero, return NULL */
+		return (NULL);
+	array = (char *) malloc(size * sizeof(char));
+	/* Allocate memoryfor the array */
 
-        array = (char *) malloc(size * sizeof(char)); /* Allocate memory for the array */
-
-        if (array == NULL) /* If memory allocation fails, return NULL */
-                return (NULL);
-
-        for (i = 0; i < size; i++)
-                array[i] = c; /* Set each element of the array to the specified char */
-
-        return (array); /* Return a pointer to the array */
-
+	if (array == NULL) /* If memory allocation fails, return NULL */
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c; /* Set each element of the array to the specified char */
+	return (array); /* Return a pointer to the array */
 }
