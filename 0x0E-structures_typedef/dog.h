12f8647a7e29274926fdca32fa67e13ba4ec69e7
rugema3 <rugema61@gmail.com>
2023-03-20 15:25:55 -0400

    my first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifindef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog gog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
+
