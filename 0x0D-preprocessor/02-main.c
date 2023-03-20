129b4f1612975b2ba77c707eb8a1db1509508789
rugema3 <rugema61@gmail.com>
2023-03-20 03:07:24 -0400

    a program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+/**
+ * main - the program that prints the name of the file
+ * it was compiled from
+ * Return: 0 (success)
+ */
+
+#include<stdio.h>
+
+int main()
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
