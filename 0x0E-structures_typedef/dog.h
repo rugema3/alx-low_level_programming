26366dfcb14cf8ef6973e9929631076bed6bff06
rugema3 <rugema61@gmail.com>
2023-03-20 17:00:32 -0400

    making corrections to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,10 @@
-#ifindef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - struct dog
- * @name: name
- * @age: age
- * @owner: owner
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
+struct dog {
+  char* name;
+  float age;
+  char* owner;
 };
 
-typedef struct dog gog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
-
+#endif /* DOG_H */
