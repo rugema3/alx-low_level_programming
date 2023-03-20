5a2706a215f8f90776d7062732d94dba61c6f7ab
rugema3 <rugema61@gmail.com>
2023-03-20 17:40:35 -0400

    betty troubles once again

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,18 +3,15 @@
 
 /**
  * print_dog - Prints a struct dog
- *
  * @d: Pointer to a struct dog
- *
  * Return: void
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
-
-    printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-    printf("Age: %.6f\n", d->age);
-    printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
 
