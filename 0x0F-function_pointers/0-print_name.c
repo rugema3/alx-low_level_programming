9d449ee8d4d70b3ff9a0712f27607e6eee4a833a
rugema3 <rugema61@gmail.com>
2023-03-22 06:06:25 -0400

    pushing my two tasks at once, the  a function that prints a name; a function that executes a function given as a parameter on each element of an array.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+/**
+ * print_name - prints a name using the provided function
+ * @name: a pointer to the name to print
+ * @f: a function that takes a char pointer
+ * as an argument and returns void
+ *
+ * This function calls the provided function
+ * pointer with the given name as an argument.
+ */
+#include <stddef.h>
+void print_name(char *name, void (*f)(char *))
+{
+	/**
+	 * The implementation checks if name and f are not null before calling f
+	 *  with name as its argument.
+	 */
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
+
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: a pointer to the array to iterate over
+ * @size: the number of elements in the array
+ * @action: a function pointer that takes an int as
+ * an argument and returns void
+ *
+ * This function iterates over each element in the array
+ * and calls the provided function
+ * with the current element as its argument.
+ */
+#include <stddef.h>
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	
+	/**
+	 * The implementation checks if array and action are not null
+	 *  before iterating over the array and calling action with each element.
+	 */
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
