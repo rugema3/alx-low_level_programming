cd835c20af47f948eeb52b50c83732e9cf698704
rugema3 <rugema61@gmail.com>
2023-03-22 16:22:08 -0400

    more bugs to fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,20 +1,18 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
 
 /**
- * main - Entry point
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: Always 0 (Success)
+ * Return: 0 on success, otherwise 98, 99 or 100
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    int a, b;
-    int (*op_func)(int, int);
-    int result;
+    int num1, num2, result;
+    int (*f)(int, int);
 
     if (argc != 4)
     {
@@ -22,24 +20,24 @@ int main(int argc, char *argv[])
         exit(98);
     }
 
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-    op_func = get_op_func(argv[2]);
+    f = get_op_func(argv[2]);
 
-    if (op_func == NULL)
+    if (f == NULL)
     {
         printf("Error\n");
         exit(99);
     }
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
     {
         printf("Error\n");
         exit(100);
     }
 
-    result = op_func(a, b);
+    result = f(num1, num2);
 
     printf("%d\n", result);
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,13 +1,11 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * op_add - returns the sum of a and b
+ * op_add - adds a and b
  * @a: first integer
  * @b: second integer
- * Return: sum of a and b
+ *
+ * Return: the result of the addition
  */
 int op_add(int a, int b)
 {
@@ -15,10 +13,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - returns the difference of a and b
+ * op_sub - subtracts b from a
  * @a: first integer
  * @b: second integer
- * Return: difference of a and b
+ *
+ * Return: the result of the subtraction
  */
 int op_sub(int a, int b)
 {
@@ -26,10 +25,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - returns the product of a and b
+ * op_mul - multiplies a and b
  * @a: first integer
  * @b: second integer
- * Return: product of a and b
+ *
+ * Return: the result of the multiplication
  */
 int op_mul(int a, int b)
 {
@@ -37,62 +37,26 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - returns the result of the division of a by b
+ * op_div - divides a by b
  * @a: first integer
  * @b: second integer
- * Return: result of the division of a by b
+ *
+ * Return: the result of the division
  */
 int op_div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
     return (a / b);
 }
 
 /**
- * op_mod - returns the remainder of the division of a by b
+ * op_mod - calculates the remainder of the division of a by b
  * @a: first integer
  * @b: second integer
- * Return: remainder of the division of a by b
+ *
+ * Return: the remainder of the division
  */
 int op_mod(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
     return (a % b);
 }
 
-/**
- * get_op_func - selects the correct function to perform the operation
- * @s: operator passed as argument to the program
- * Return: pointer to the function that corresponds to the operator given
- */
-int (*get_op_func(char *s))(int, int)
-{
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-
-    i = 0;
-    while (ops[i].op != NULL)
-    {
-        if (strcmp(ops[i].op, s) == 0)
-        {
-            return (ops[i].f);
-        }
-        i++;
-    }
-    return (NULL);
-}
