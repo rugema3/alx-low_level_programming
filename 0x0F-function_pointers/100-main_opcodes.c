74794854ac280bb8428a0204bc009957a1c1b0ef
rugema3 <rugema61@gmail.com>
2023-03-22 14:50:29 -0400

    betty issues

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,31 +12,26 @@
  */
 int main(int argc, char *argv[])
 {
-    int num_bytes, i;
-    char *func_ptr;
+	int num_bytes, i;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <num_bytes>\n", argv[0]);
-        return 1;
-    }
+	char *func_ptr;
 
-    num_bytes = atoi(argv[1]);
-
-    if (num_bytes <= 0) {
-        fprintf(stderr, "Error: num_bytes must be a positive integer\n");
-        return 2;
-    }
-
-    func_ptr = (char *) main;
-
-    printf("%p:\n", (void *) func_ptr);
-
-    for (i = 0; i < num_bytes; i++) {
-        printf("  %.2hhx ", func_ptr[i]);
-    }
-
-    printf("\n");
-
-    return 0;
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <num_bytes>\n", argv[0])
+			return (1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes <= 0)
+	{
+		fprintf(stderr, "Error: num_bytes must be a positive integer\n");
+		return (2);
+	}
+	func_ptr = (char *) main;
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%.2hhx ", func_ptr[i]);
+	}
+	printf("\n");
+	return (0);
 }
-
