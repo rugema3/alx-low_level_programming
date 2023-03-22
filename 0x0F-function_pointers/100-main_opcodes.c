cbd35225892d6bc86879de90fa605188e8b4018c
rugema3 <rugema61@gmail.com>
2023-03-22 15:04:57 -0400

    betty compliance

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,31 +11,25 @@
  */
 int main(int argc, char *argv[])
 {
-        int num_bytes, i;
-        char *func_ptr;
+	int num_bytes, i;
+	char *func_ptr;
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                return (1);
-        }
-
-        num_bytes = atoi(argv[1]);
-
-        if (num_bytes < 0)
-        {
-                printf("Error\n");
-                return (2);
-        }
-
-        func_ptr = (char *)main;
-
-        for (i = 0; i < num_bytes; i++)
-        {
-                printf("%.2hhx ", func_ptr[i]);
-        }
-
-        printf("\n");
-
-        return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	func_ptr = (char *)main;
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%.2hhx ", func_ptr[i]);
+	}
+	printf("\n");
+	return (0);
 }
