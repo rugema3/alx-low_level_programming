c23e534f0026fea0b41b26d41f98dedf26fb6be3
rugema3 <rugema61@gmail.com>
2023-03-22 14:53:56 -0400

    more bugs

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * main - Prints the opcodes of its own main function
  *
- * @argc: The number of command-line arguments passed to the program
+ * @argc: The number of arguments passed to the program
  * @argv: An array of pointers to the arguments
  *
  * Return: 0 if successful, 1 if the number of arguments is incorrect,
@@ -12,26 +9,31 @@
  */
 int main(int argc, char *argv[])
 {
-	int num_bytes, i;
+        int num_bytes, i;
+        char *func_ptr;
 
-	char *func_ptr;
+        if (argc != 2)
+        {
+                printf("Error\n");
+                return (1);
+        }
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <num_bytes>\n", argv[0])
-			return (1);
-	}
-	num_bytes = atoi(argv[1]);
-	if (num_bytes <= 0)
-	{
-		fprintf(stderr, "Error: num_bytes must be a positive integer\n");
-		return (2);
-	}
-	func_ptr = (char *) main;
-	for (i = 0; i < num_bytes; i++)
-	{
-		printf("%.2hhx ", func_ptr[i]);
-	}
-	printf("\n");
-	return (0);
+        num_bytes = atoi(argv[1]);
+
+        if (num_bytes < 0)
+        {
+                printf("Error\n");
+                return (2);
+        }
+
+        func_ptr = (char *)main;
+
+        for (i = 0; i < num_bytes; i++)
+        {
+                printf("%.2hhx ", func_ptr[i]);
+        }
+
+        printf("\n");
+
+        return (0);
 }
