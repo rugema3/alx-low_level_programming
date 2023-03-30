43ada779ccfe411ceb95436a6cd652d823562e45
rugema3 <rugema61@gmail.com>
2023-03-30 07:51:13 -0400

    a function that prints before the main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * print_message - prints a message before the main function is executed.
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+
