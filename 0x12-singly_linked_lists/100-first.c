d30599c20bfde9d7790a1748c9ba9e62a595f0db
rugema3 <rugema61@gmail.com>
2023-03-30 15:08:13 -0400

    constructor attribute, which tells the compiler to execute this function before main is called.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,8 +2,10 @@
 
 /**
  * print_message - prints a message before the main function is executed.
+ * the print_message function is marked with the constructor attribute,
+ * which tells the compiler to execute this function before main is called.
  */
-void print_message(void)
+void __attribute__((constructor)) print_message(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
