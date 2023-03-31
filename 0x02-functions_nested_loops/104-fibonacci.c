ba509c7fd052765faa28bc3261dd9cec46cb78f7
rugema3 <rugema61@gmail.com>
2023-03-31 15:53:53 -0400

    a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,21 +1,34 @@
-/**
- * main - the program prints the first 98 numbers
- * starting with 1 and 2
- * Return: 0 ( success)
- */
 #include<stdio.h>
+/**
+ * main - the program prints the first 98 fibonacci numbers
+ * staring with 1 and 2.
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	unsigned int fib1 = 1, i, fib2 = 2;
+	int i;
+	double fibo[98] = {1, 2};
+	/**
+	 *  A double is used because an int would not hold large numbers.
+	 *  The fibonacci series gets larger pretty quick
+	 */
+	printf("***************************************************************\n");
+	printf("fibo[0]:\t %.0f\n", fibo[0]);
+	/* I want the double to come with no decimal places*/
+	printf("fibo[1]:\t %.0f \n", fibo[1]);
+for (i = 2; i < 98; i++)
+{
 
-	printf("%d\n%d\n", fib1, fib2);
-	for (i = 2; i < 98; i++)
-	{
-		unsigned int fib = fib1 + fib2;
+	fibo[i] = fibo[i - 2] + fibo[i - 1];
+	/**
+	 * the fibonacci series is obtained by add the obtained
+	 * number from the previous number
+	 */
+	printf("fibo[%d]:\t %.0f\n", i, fibo[i]);
 
-		fib1 = fib2;
-		fib2 = fib;
-		printf("%d\n", fib);
-	}
-	return (0);
 }
+printf("---------------------------------------------------------\n");
+
+return (0);
+}
+
