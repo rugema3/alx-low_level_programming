4f3515730571bb94be69658a3fac1ad79043bcf4
rugema3 <rugema61@gmail.com>
2023-04-04 04:50:57 -0400

    pushing

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,5 @@
-#include"lists.h"
+#include "lists.h"
 #include<stdio.h>
-
 /**
  * print_listint_safe - prints a listint_t linked list safely
  * @head: pointer to the head of the linked list
@@ -8,27 +7,23 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *curr = head;
-    const listint_t *next_node = NULL;
-    size_t node_count = 0;
+	const listint_t *curr = head;
+	const listint_t *next_node = NULL;
+	size_t node_count = 0;
 
-    while (curr)
-    {
-        node_count++;
-        printf("[%p] %d\n", (void *)curr, curr->n);
-        next_node = curr->next;
-
-        /* check if next node has been visited before */
-        if (next_node >= curr)
-        {
-            printf("-> [%p] %d\n", (void *)next_node, next_node->n);
-            printf("Linked list has a cycle\n");
-            break;
-        }
-
-        curr = next_node;
-    }
-
-    return (node_count);
+	/* traverse the linked list */
+	while (curr)
+	{
+		node_count++;
+		printf("[%p] %d\n", (void *)curr, curr->n);
+		next_node = curr->next;
+		/* check if next node has been visited before */
+		if (next_node >= curr)
+		{
+			printf("-> [%p] %d\n", (void *)next_node, next_node->n);
+			break;
+		}
+		curr = next_node;
+	}
+	return (node_count);
 }
-
