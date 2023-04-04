3f7afdd0b3e793381e9ac3592841383de52e4b33
rugema3 <rugema61@gmail.com>
2023-04-04 02:08:17 -0400

    hassling with the input

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,12 +1,10 @@
 #include"lists.h"
-
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
+ * insert_nodeint_at_index - adds a new node at a given position in a list
  * @head: pointer to the head of the list
  * @idx: index of the list where the new node should be added (starting at 0)
- * @n: value of the new node
- *
- * Return: the address of the new node, or NULL if it failed
+ * @n: value to store in the new node
+ * Return: pointer to the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -29,15 +27,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	current_node = *head;
-	for (i = 0; i < idx - 1; i++) {
-		if (current_node == NULL || current_node->next == NULL)
-			return (NULL);
+	for (i = 0; current_node != NULL && i < idx - 1; i++) {
 		current_node = current_node->next;
 	}
-
+	if (current_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->next = current_node->next;
 	current_node->next = new_node;
-
 	return (new_node);
 }
 
