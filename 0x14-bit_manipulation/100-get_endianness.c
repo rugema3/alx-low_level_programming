300422aa33d45d9b9b75047660d3f0abc649f231
rugema3 <rugema61@gmail.com>
2023-04-08 03:45:10 -0400

    pushing the files at the same time

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+  /* Create a multi-byte value with a known pattern */
+  unsigned int value = 0x01;
+
+  /* Shift the value to the right to isolate the first byte */
+  unsigned char byte = (unsigned char)(value >> (sizeof(unsigned int) - 1) * 8);
+
+  /* Check the value of the first byte to determine the endianness */
+  if (byte == 0x01) {
+    /* If the first byte is 0x01, it means the system is little endian */
+    return 1; /* little endian */
+  } else {
+    /* Otherwise, the system is big endian */
+    return 0; /* big endian */
+  }
+}
+
