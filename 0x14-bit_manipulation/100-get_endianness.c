d7cdd6a18e357d6834c864638d04942ea704dc42
rugema3 <rugema61@gmail.com>
2023-04-08 03:49:33 -0400

    complying with betty

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,19 +5,22 @@
  */
 int get_endianness(void)
 {
-  /* Create a multi-byte value with a known pattern */
-  unsigned int value = 0x01;
+	/* Create a multi-byte value with a known pattern */
+	unsigned int value = 0x01;
 
-  /* Shift the value to the right to isolate the first byte */
-  unsigned char byte = (unsigned char)(value >> (sizeof(unsigned int) - 1) * 8);
+	/* Shift the value to the right to isolate the first byte */
+	unsigned char byte = (unsigned char)(value >> (sizeof(unsigned int) - 1) * 8);
+	/* Check the value of the first byte to determine the endianness */
 
-  /* Check the value of the first byte to determine the endianness */
-  if (byte == 0x01) {
-    /* If the first byte is 0x01, it means the system is little endian */
-    return 1; /* little endian */
-  } else {
-    /* Otherwise, the system is big endian */
-    return 0; /* big endian */
-  }
+	if (byte == 0x01)
+	{
+		/* If the first byte is 0x01, it means the system is little endian */
+		return (1); /* little endian */
+	}
+	else
+	{
+		/* Otherwise, the system is big endian */
+		return (0); /* big endian */
+	}
 }
 
