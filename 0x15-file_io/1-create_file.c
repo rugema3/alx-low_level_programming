fe4206df8ca9942b5ab7c8225f8c0df3a8d184c6
rugema3 <rugema61@gmail.com>
2023-04-10 04:36:11 -0400

    errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -45,7 +45,6 @@ int create_file(const char *filename, char *text_content)
 		 * count is an integer variable that holds the return value of
 		 * the write system call, which represents the number of bytes
 		 * actually written to the file.
-		/*
 		 * Write text_content to the file
 		 * Note: write returns the number of bytes written,
 		 * which should be equal to len
