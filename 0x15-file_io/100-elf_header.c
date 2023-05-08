1e2447d8757145efbe6ccdb8729a06ea3cd62b6e
rugema3 <rugema61@gmail.com>
2023-05-08 17:31:54 -0400

    modification

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,66 +1,199 @@
-#include <elf.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 /**
- * main - program that displays the ELF header at the start of an ELF file
- * @argc: argument count
- * @argv: argument vector
+ * print_error - prints error message and exits
+ * @msg: error message to print
  *
- * Return: 0 on success, 98 on error
+ * Return: void
+ */
+void print_error(const char *msg)
+{
+	fprintf(stderr, "Error: %s\n", msg);
+	exit(98);
+}
+/**
+ * print_magic - prints ELF magic number
+ * @magic: ELF magic number
+ *
+ * Return: void
+ */
+void print_magic(const unsigned char *magic)
+{
+	int i;
+
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x%c", magic[i], i == EI_NIDENT - 1 ? '\n' : ' ');
+}
+/**
+ * print_class - prints ELF class
+ * @elf_class: ELF class
+ *
+ * Return: void
+ */
+void print_class(uint8_t elf_class)
+{
+	printf("  Class:                             %s\n",
+			elf_class == ELFCLASS32 ? "ELF32" : "ELF64");
+}
+/**
+ * print_data - prints ELF data encoding
+ * @elf_data: ELF data encoding
+ *
+ * Return: void
+ */
+void print_data(uint8_t elf_data)
+{
+	printf("  Data:                              %s\n",
+			elf_data == ELFDATA2LSB ? "2's complement, little endian" :
+			"2's complement, big endian");
+}
+/**
+ * print_version - prints ELF version
+ * @elf_version: ELF version
+ *
+ * Return: void
+ */
+void print_version(uint8_t elf_version)
+{
+	printf("  Version:                           %d (current)\n", elf_version);
+}
+/**
+ * print_osabi - prints ELF OS/ABI
+ * @elf_osabi: ELF OS/ABI
+ *
+ * Return: void
+ */
+void print_osabi(uint8_t elf_osabi)
+{
+	printf("  OS/ABI:                            ");
+	switch (elf_osabi)
+	{
+		case ELFOSABI_SYSV:
+			printf("UNIX - System V\n");
+			break;
+		case ELFOSABI_HPUX:
+			printf("UNIX - HP-UX\n");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD\n");
+			break;
+		case ELFOSABI_LINUX:
+			printf("UNIX - Linux\n");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("UNIX - Solaris\n");
+			break;
+
+		case ELFOSABI_IRIX:
+			printf("UNIX - IRIX\n");
+			break;
+		case ELFOSABI_FREEBSD:
+			printf("UNIX - FreeBSD\n");
+			break;
+		case ELFOSABI_TRU64:
+			printf("UNIX - TRU64\n");
+			break;
+		case ELFOSABI_ARM:
+			printf("ARM\n");
+			break;
+		case ELFOSABI_STANDALONE:
+			printf("Standalone App\n");
+			break;
+		default:
+			printf("<unknown: %d>\n", elf_osabi);
+			break;
+	}
+}
+/**
+ * print_type - prints ELF file type
+ * @elf_type: ELF file type
+ *
+ * Return: void
+ */
+void print_type(uint16_t elf_type)
+{
+	printf("  Type:                              ");
+	switch (elf_type)
+	{
+		case ET_NONE:
+			printf("NONE (None)\n");
+			break;
+		case ET_REL:
+			printf("REL (Relocatable file)\n");
+			break;
+		case ET_EXEC:
+			printf("EXEC (Executable file)\n");
+			break;
+		case ET_DYN:
+			printf("DYN (Shared object file)\n");
+			break;
+		case ET_CORE:
+			printf("CORE (Core file)\n");
+			break;
+		default:
+			printf("<unknown: %d>\n", elf_type);
+			break;
+	}
+}
+/**
+ * print_entry - prints ELF entry point address
+ * @elf_entry: ELF entry point address
+ *
+ * Return: void
+ */
+void print_entry(uint64_t elf_entry)
+{
+	printf("  Entry point address:               0x%lx\n", elf_entry);
+}
+/**
+ * print_header - prints ELF header information
+ * @filename: filename of ELF file to read
+ *
+ * Return: void
+ */
+void print_header(const char *filename)
+{
+	int fd;
+	Elf64_Ehdr ehdr;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		print_error("Could not open file");
+
+	if (lseek(fd, 0, SEEK_SET) == -1)
+		print_error("Could not seek to beginning of file");
+
+	if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
+		print_error("Could not read ELF header");
+
+	if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0)
+		print_error("File is not an ELF file");
+
+	printf("ELF Header:\n");
+	print_magic(ehdr.e_ident);
+	print_class(ehdr.e_ident[EI_CLASS]);
+	print_data(ehdr.e_ident[EI_DATA]);
+	print_version(ehdr.e_ident[EI_VERSION]);
+	print_osabi(ehdr.e_ident[EI_OSABI]);
+	printf("  ABI Version:                       %d\n", ehdr.e_ident[EI_ABIVERSION]);
+	print_type(ehdr.e_type);
+	print_entry(ehdr.e_entry);
+
+	close(fd);
+}
+/**
+ * main - prints ELF header information
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-	int fd, i;
-	ssize_t bytes_read;
-	Elf64_Ehdr ehdr;
-
 	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-		return (98);
-	}
+		print_usage_error();
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		perror("Error opening file");
-		return (98);
-	}
+	print_header(argv[1]);
 
-	bytes_read = read(fd, &ehdr, sizeof(ehdr));
-	if (bytes_read != sizeof(ehdr))
-	{
-		perror("Error reading ELF header");
-		close(fd);
-		return (98);
-	}
-	if (ehdr.e_ident[EI_MAG0] != ELFMAG0 || ehdr.e_ident[EI_MAG1] != ELFMAG1 ||
-			ehdr.e_ident[EI_MAG2] != ELFMAG2 || ehdr.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		fprintf(stderr, "Error: Not an ELF file\n");
-		close(fd);
-		return (98);
-	}
-	/* Display the ELF header */
-	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-		printf("%02x ", ehdr.e_ident[i]);
-	printf("\n");
-	printf("  Class:                             %s\n",
-			ehdr.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n",
-			ehdr.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", ehdr.e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %s\n", ehdr.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
-	printf("  ABI Version:                       %d\n", ehdr.e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %s\n",
-			ehdr.e_type == ET_EXEC ? "EXEC (Executable file)" : "Other");
-	printf("  Entry point address:               0x%lx\n", ehdr.e_entry);
-	close(fd);
 	return (0);
 }
 
