a4282bb324e64cd3051137d0c40efa32fac3c45b
rugema3 <rugema61@gmail.com>
2023-06-15 19:04:09 +0200

    Using a different approach for the program to run

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,8 +1,9 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position
- * in a dlistint_t linked list
+ * insert_dnodeint_at_index - Inserts a new node at
+ * a given position in a dlistint_t linked list
  * @h: Double pointer to the head of the list
  * @idx: Index where the new node should be added
  * @n: Data value of the new node
@@ -11,19 +12,36 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	if (h == NULL || idx > dlistint_len(*h))
+	dlistint_t *new_node, *temp;
+	unsigned int i;
+
+	if (h == NULL)
 		return (NULL);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	if (*h == NULL)
+	temp = *h;
+
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+
+	if (temp == NULL)
 		return (NULL);
 
-	(*h)->next = insert_dnodeint_at_index(&((*h)->next), idx - 1, n);
-	if ((*h)->next != NULL)
-		(*h)->next->prev = *h;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-	return (*h);
+	new_node->n = n;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+
+	temp->next = new_node;
+
+	return (new_node);
 }
 
