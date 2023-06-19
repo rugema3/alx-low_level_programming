cbe9774f414ce5a7c164ff9b7c14388b9bf2f71b
rugema3 <rugema61@gmail.com>
2023-06-19 13:31:08 +0200

    a dynamic library containg functions that are called by python

---
+++ 0x18-dynamic_libraries/my_functions.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+/**
+ * add - a function that adds two integers
+ * @a: integer for operations
+ * @b: integer for operation
+ * Return: sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - a function that subtracts integers
+ * @a: integer to operate the operation on
+ * @b: integer to operate an operation on.
+ * Return: the difference of two integers.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - a function that multiplies two integers
+ * @a: integers to operate with
+ * @b: integer to operate with
+ * Return: the product of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - a function that divides two integers
+ * @a: integer to operate with
+ * @b: integer to operate with
+ * Return: The quotient of two integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - a function that computes the modulus of two integers
+ * @a: integer to operate with
+ * @b: integer to operate with
+ * Return: the modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
