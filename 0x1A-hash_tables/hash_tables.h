0f0cf07ecb535a60fb88c685b41738ea2d08d1b2
rugema3 <rugema61@gmail.com>
2023-07-11 09:51:46 +0200

    implementing the djb2 algorithm

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include<stdio.h>
+#include<stdlib.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -33,6 +36,7 @@ typedef struct hash_table_s
 
 /* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
 
